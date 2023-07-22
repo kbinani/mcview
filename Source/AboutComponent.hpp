9450d8a2841600eeab5e9ea99ec7d63597faaa6e
kbinani <kbinani.bt@gmail.com>
2023-07-23 03:42:14 +0900

    Make acknowledgement text scrollable

--- Source/AboutComponent.hpp
+++ Source/AboutComponent.hpp
@@ -3,41 +3,96 @@
 namespace mcview {
 
 class AboutComponent : public juce::Component {
+  class Content : public juce::Component {
+  public:
+    Content() {
+      fLines = {
+          CreateLabel(juce::String("Version: ") + juce::String::fromUTF8(JUCE_APPLICATION_VERSION_STRING)),
+          CreateLabel("Copyright (C) 2019, 2022, 2023 kbinani"),
+          CreateLabel(""),
+          CreateLabel("Acknowledgement"),
+          CreateLabel(""),
+
+          CreateLabel("JUCE"),
+          CreateLink("https://github.com/juce-framework/JUCE"),
+          CreateLink("https://github.com/kbinani/JUCE"),
+          CreateLabel(""),
+
+          CreateLabel("Google Material Design Icon"),
+          CreateLink("https://material.io/tools/icons"),
+          CreateLabel(""),
+
+          CreateLabel("colormap-shaders"),
+          CreateLink("https://github.com/kbinani/colormap-shaders"),
+          CreateLabel(""),
+
+          CreateLabel("libminecraft-file"),
+          CreateLink("https://github.com/kbinani/libminecraft-file"),
+          CreateLabel(""),
+
+          CreateLabel("Android SDK Sources"),
+          CreateLink("https://github.com/AndroidSDKSources/android-sdk-sources-for-api-level-28"),
+          CreateLabel(""),
+
+          CreateLabel("Noto Sans"),
+          CreateLink("https://fonts.google.com/noto/specimen/Noto+Sans"),
+          CreateLabel(""),
+
+          CreateLabel("Noto Sans JP"),
+          CreateLink("https://fonts.google.com/noto/specimen/Noto+Sans+JP"),
+          CreateLabel(""),
+
+          CreateLabel("Noto Sans TC"),
+          CreateLink("https://fonts.google.com/noto/specimen/Noto+Sans+TC"),
+          CreateLabel(""),
+
+          CreateLabel("Noto Sans SC"),
+          CreateLink("https://fonts.google.com/noto/specimen/Noto+Sans+SC"),
+          CreateLabel(""),
+      };
+      for (auto const &line : fLines) {
+        addAndMakeVisible(*line);
+      }
+    }
+
+    void resized() override {
+      int const width = getWidth();
+      int y = 0;
+      for (auto const &line : fLines) {
+        line->setBounds(5, y, width - 10, line->getHeight());
+        y += line->getHeight();
+      }
+    }
+
+    int getPreferredHeight() const {
+      int h = 0;
+      for (auto const &line : fLines) {
+        h += line->getHeight();
+      }
+      return h;
+    }
+
+  private:
+    juce::Array<std::unique_ptr<Component>> fLines;
+  };
+
 public:
   AboutComponent() {
     using namespace juce;
-    fLines = {
-        CreateLabel(String("Version: ") + String::fromUTF8(JUCE_APPLICATION_VERSION_STRING)),
-        CreateLabel("Copyright (C) 2019, 2022, 2023 kbinani"),
-        CreateLabel(""),
-        CreateLabel("Acknowledgement"),
-        CreateLabel(""),
-
-        CreateLabel("JUCE"),
-        CreateLink("https://github.com/juce-framework/JUCE"),
-        CreateLink("https://github.com/kbinani/JUCE"),
-        CreateLabel(""),
-
-        CreateLabel("Google Material Design Icon"),
-        CreateLink("https://material.io/tools/icons"),
-        CreateLabel(""),
-
-        CreateLabel("colormap-shaders"),
-        CreateLink("https://github.com/kbinani/colormap-shaders"),
-        CreateLabel(""),
-
-        CreateLabel("libminecraft-file"),
-        CreateLink("https://github.com/kbinani/libminecraft-file"),
-        CreateLabel(""),
-
-        CreateLabel("Android SDK Sources"),
-        CreateLink("https://github.com/AndroidSDKSources/android-sdk-sources-for-api-level-28"),
-    };
-    for (auto const &line : fLines) {
-      addAndMakeVisible(*line);
-    }
     fLogo = Drawable::createFromImageData(BinaryData::icon_large_png, BinaryData::icon_large_pngSize);
+    fContent.reset(new Content);
+    fViewport.reset(new juce::Viewport);
+    fViewport->setViewedComponent(fContent.get(), false);
+    fViewport->setScrollBarsShown(true, false);
+    addAndMakeVisible(fViewport.get());
     setSize(400, 500);
+  }
+
+  void resized() override {
+    auto bounds = getLocalBounds();
+    bounds.removeFromTop(190);
+    fContent->setSize(bounds.getWidth(), fContent->getPreferredHeight());
+    fViewport->setBounds(bounds);
   }
 
   void paint(juce::Graphics &g) override {
@@ -68,20 +123,17 @@ public:
       g.drawText(JUCE_APPLICATION_NAME_STRING, margin, y, width - 2 * margin, titleHeight, Justification::centred);
       y += titleHeight + margin;
     }
-    for (auto const &line : fLines) {
-      line->setBounds(0, y, width, line->getHeight());
-      y += line->getHeight();
-    }
   }
 
 private:
-  static juce::Component *CreateLabel(juce::String t, int height = 14) {
+  static juce::Component *CreateLabel(juce::String t, int height = 16) {
     using namespace juce;
     Label *l = new Label();
     l->setText(t, NotificationType::dontSendNotification);
     l->setBounds(0, 0, 100, height);
     l->setColour(Label::textColourId, Colours::white);
     l->setJustificationType(Justification::centred);
+    l->setMinimumHorizontalScale(0);
     return l;
   }
 
@@ -99,7 +151,8 @@ private:
 
 private:
   std::unique_ptr<juce::Drawable> fLogo;
-  juce::Array<std::unique_ptr<Component>> fLines;
+  std::unique_ptr<Content> fContent;
+  std::unique_ptr<juce::Viewport> fViewport;
 };
 
 } // namespace mcview
